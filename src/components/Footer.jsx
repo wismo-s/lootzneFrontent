e88f2ed9699272491a75249dc7e9a0c4eab3b30c
wismo-s <stepha2128.af@gmail.com>
2025-05-06 13:00:45 -0500

    edicion al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 export default function Footer() {
   const social = {
-    wismo: "https://github.com/wismo-s",
+    frontend: "https://github.com/wismo-s",
+    backend: "https://github.com/wismo-s",
   };
   return (
     <footer>
-      <a href={social.wismo} target="_blank" rel="noopener noreferrer">
-        Hecho por Team Guarmain
+      <a href={social.frontend} target="_blank" rel="noopener noreferrer">
+        frontend repo
+      </a>
+      <a href={social.backend} target="_blank" rel="noopener noreferrer">
+        backend repo
       </a>
     </footer>
   );
